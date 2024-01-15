0c3b51b8d34584083fd6397afd03fd7b747abfc6
naoya-iida <133567492+naoya-iida@users.noreply.github.com>
2024-01-15 20:05:07 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ st.write("プランの文章を考えるのが大好きな3人がそれぞれの
 
  # ラジオボタンの作成
 selected_option = st.radio("プランの文体を選んでください", ["ホテルの支配人", "ギャル", "旅館の女将"])
+selected_optioncall = ""  # selected_optioncall を初期化
 if selected_option:
     st.write(f"{selected_option}がプランを作ってくれます。{selected_optioncall}")
 
