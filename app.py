685ad3010744963459e52cd5da4f4a6a8d32e029
naoya-iida <133567492+naoya-iida@users.noreply.github.com>
2024-01-19 13:27:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,8 @@ def communicate():
     messages.append(user_message)
 
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4",
+    # model="gpt-3.5-turbo",
     messages=messages
      )
     # response = client.chat.completions.create(model="gpt-3.5-turbo",
